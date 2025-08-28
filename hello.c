4a50920630d547bcbc1ca09a0711afb3d1cdca34
Dillon Jurjevich <djurjevich2@huskers.unl.edu>
2025-08-28 09:55:16 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ *
+ *  Author: Dillon Jurjevich
+ *          djurjevich2@huskers.unl.edu
+ *  Date: 8/28/25
+ *
+ *  A hello program that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)   {
+
+printf("Dillon Jurjevich\n");
+
+}
